b1f01a242961057a11110dc3cda8535bb5e478bb
Tulasi1005 <ravirv7147@gmail.com>
2026-08-16 11:05:23 +0530

    App commit

--- src/components/Home/subgroup.js
+++ src/components/Home/subgroup.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import Navbar from "../../Navbar";
+// import Navbar from "../../Navbar";
 import nexergyLogo from "../../GK_NEXERGY_4.png";
 import "./subgroup.css";
 
